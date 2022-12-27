7d76d84506e7498eda008bf4f2dee48151806aa5
ajadreddy007 <ajad.edavelly@testaing.com>
2022-12-27 21:21:47 +0530

    added code

--- Arrays/kth-max-n-min-in-an-arr.cpp
+++ Arrays/kth-max-n-min-in-an-arr.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    cout<< 1;
-}
+int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        int n=r+1;
+        sort(arr,arr+n);
+        return arr[k-1];
+    }

cd66ff298db0a2289098bc3729738560edf45e9a
ajadreddy <78726978+ajadreddy@users.noreply.github.com>
2022-12-27 20:47:05 +0530

    Update Reverse the array.cpp

--- Arrays/Reverse the array.cpp
+++ Arrays/Reverse the array.cpp
@@ -1,1 +1,12 @@
 
+string reverseWord(string s)
+{
+    char temp;
+    int n=s.size()-1;
+    for(int i=0;i<=(n/2);i++){
+        temp=s[i];
+        s[i]=s[n-i];
+        s[n-i]=temp;
+    }
+  return s;
+}
